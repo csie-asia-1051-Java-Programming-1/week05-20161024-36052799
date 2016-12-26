d29290e16c66fb3360a7d674a2e2e2e5be17e6e4
36052799 <105021035@live.asia.edu.tw>
2016-12-26 15:47:14 +0800

    Update hw03.java

--- src/hw/hw03.java
+++ src/hw/hw03.java
@@ -1,10 +1,18 @@
 package hw;
-
+import java.util.Scanner;
 public class hw03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int count = 0;
+		int sum = 1;
+		while (sum<=10000){
+			count ++;
+			sum = sum*n;
+			
+  }
+		System.out.print("次方次數"+count);
 	}
-
 }
